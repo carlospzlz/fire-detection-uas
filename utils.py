506e130d1bd641a98150adc3382d1f6f1aaeb218
Carlos Perez-Lopez <carlospzlz@gmail.com>
2019-10-13 01:10:38 +0100

    Seems working!

--- utils.py
+++ utils.py
@@ -7,3 +7,15 @@ def read_image_with_alpha(file_):
     b_channel, g_channel, r_channel = cv2.split(img)
     alpha_channel = np.ones(b_channel.shape, dtype=b_channel.dtype) * 255
     return cv2.merge((b_channel, g_channel, r_channel, alpha_channel))
+
+
+def blend(img1, img2):
+    b1, g1, r1, alpha1 = cv2.split(img1)
+    b2, g2, r2, alpha2 = cv2.split(img2)
+    alpha = alpha2.astype(float) / 255
+    b = b2 * alpha + b1 * (1 - alpha)
+    g = g2 * alpha + g1 * (1 - alpha)
+    r = r2 * alpha + r1 * (1 - alpha)
+    alpha = (alpha1 + alpha2) // 2
+    return cv2.merge(
+        (b.astype(np.uint8), g.astype(np.uint8), r.astype(np.uint8), alpha))
