d1da2f70f1fc961681d28165fcd7d76035f0a22a
Carlos Perez-Lopez <carlospzlz@gmail.com>
2019-10-21 20:49:39 +0100

    Added GPL-3.0 license

--- utils.py
+++ utils.py
@@ -1,3 +1,21 @@
+#
+#    This file is part of Fire Detection UAS.
+#
+#    Fire Detection UAS is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Fire Detection UAS is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Fire Detection UAS.  If not, see
+#    <https://www.gnu.org/licenses/>.
+#
+
 from enum import Enum
 
 import numpy as np
